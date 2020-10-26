ab5815897540c5a331c0a749ae3c0f26e8b9f169
Maura <mwitzer21@holynames-sea.org>
2020-10-26 07:49:59 -0700

    Created html links that takes each subject and links to a different page

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "hello";
+	@GetMapping("/subject")
+	public String subject(@RequestParam(name="subject", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("subject", name);
+		return "subject";
 	}
 
 }
